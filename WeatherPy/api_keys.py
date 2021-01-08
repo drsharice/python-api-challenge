dafbbf1459979ef8814978409655694a12948f0e
drsharice <drsharice@scannady.net>
2021-01-07 20:07:39 -0500

    Trying to push

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,14 @@
+# OpenWeatherMap API Key
+weather_api_key = "689b46727b951bd2fc710852babc0790"
+
+weather2_api_key = "176a37e06bf2978a132ca5d48be4c255"
+
+# Google API Key
+g_key = "AIzaSyCNVAHWOhQ8oyP032PiORC7kpUHTk--crM"
+
+g2_key = "AIzaSyAXamAz2d9ReywedciC9iG4Ao56BmFVrwY"
+
+g3_key = "AIzaSyD3RbGYnAYaksGofh00oAvb7j5LzjuYGq8"
+
+g4_key = "AIzaSyBJjmgL5RzMoSBER9wJSYzOax-kSV4h3h0"
+
